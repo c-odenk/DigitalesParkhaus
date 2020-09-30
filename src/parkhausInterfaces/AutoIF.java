266289a33246d6e9fcc1092b78e4671f9ee8800a
eturk28 <emre.tuerker@smail.inf.h-brs.de>
2020-09-30 19:40:12 +0200

    Klasse für Autos + Tests

---
+++ src/parkhausInterfaces/AutoIF.java
@@ -0,0 +1,12 @@
+package parkhausInterfaces;
+
+public interface AutoIF 
+{
+	public int getParkplatz();
+	
+	public String getTicket();
+	
+	public void setParkplatz(int p);
+	
+	public void setTicket(String t);
+}
